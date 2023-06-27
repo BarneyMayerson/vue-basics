a92396e2c19365d82c2391f4f83c79dfd5967633
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-06-27 16:12:04 +0300

    02-018 Binding Classes (wip binding an array)

--- app.js
+++ app.js
@@ -1,46 +1,14 @@
-const vm = Vue.createApp({
+let vm = Vue.createApp({
     data() {
         return {
-            firstName: "Joe",
-            middleName: "",
-            lastName: "Doe",
-            url: "https://google.com",
-            raw_url: "<a href='https://google.com' target='_blanc'>Google</a>",
-            age: 25,
+            isPurple: false,
+            selectedColor: "",
         }
-    },
-
-    methods: {
-        increment() {
-            this.age++;
-        },
-
-        updateLastName(msg, event) {
-            console.log(msg);
-
-            this.lastName = event.target.value;
-        },
-
-        updateMiddleName(event) {
-            this.middleName = event.target.value;
-        },
     },
 
     computed: {
-        fullName() {
-            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
-        },
-    },
-
-    watch: {
-        age(newVal, oldVal) {
-            setTimeout(() => {
-                this.age = 25;
-            }, 3000)
+        circleClasses() {
+            return { purple: this.isPurple };
         }
     }
-}).mount("#app");
-
-setTimeout(() => {
-    vm.firstName = "Bobby";
-}, 2000);
+}).mount('#app')
